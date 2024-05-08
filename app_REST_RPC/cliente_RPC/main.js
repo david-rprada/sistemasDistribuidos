8a3989b2aee3ffd838637b4975a3bfacd02089c3
anamarzal <163570372+anamarzal@users.noreply.github.com>
2024-05-08 12:41:21 +0200

    Add files via upload

--- app_REST_RPC/cliente_RPC/main.js
+++ app_REST_RPC/cliente_RPC/main.js
@@ -11,9 +11,14 @@ const obtenerEspecialidades = app.procedure("obtenerEspecialidades");
 const obtenerDatosMedico = app.procedure("obtenerDatosMedico");
 const crearME = app.procedure("crearME");
 const actualizarme = app.procedure("actualizarme");
+const obtenerExpDisponibles = app.procedure("obtenerExpDisponibles");
+const asignarExp = app.procedure("asignarExp");
+const obtenerExpAsignados = app.procedure("obtenerExpAsignados");
+const resolverExp = app.procedure("resolverExp");
 
-//var seccionActual = "login_me";
-var id_ME = null;
+var id_medico = null;
+var expedientesAsignados=[];
+
 
 //las funciones se hacen de la siguiente manera: 
 /*Tenemos que tener en cuenta los botones como en la parte del REST
@@ -45,6 +50,7 @@ function entrar(){
             console.log("Login incorrecto, pruebe otra vez");
         }    
     });
+    borrar_campos_login();
 }
 
 function datos_especialista(datos_ME){
@@ -62,14 +68,6 @@ function datos_especialista(datos_ME){
         } 
     })  
 }
-
-
-function registrarse(){
-    cargarCentros();
-    cargarEspecialidades();
-    cambiarSeccion("registro_nuevo_ME");
-}
-
 // Esta función carga los centros en el select
 function cargarCentros() {
     obtenerCentros(function(centros) {
@@ -102,6 +100,11 @@ function cargarEspecialidades() {
     });
 }
 
+function registrarse(){
+    cargarCentros();
+    cargarEspecialidades();
+    cambiarSeccion("registro_nuevo_ME");
+}
 
 function guardar_nuevo_me(){
     var centro_seleccionado = document.getElementById("centros");
@@ -125,17 +128,30 @@ function guardar_nuevo_me(){
     crearME(nuevo_me, function(id_nuevoME){
         cambiarSeccion("login_me");
         console.log("Este es el id del nuevo ME:", id_nuevoME);
+        borrar_campos_registro();
     });
 }
 
+function boton_volver_login(){
+    cambiarSeccion("login_me");
+}
+
 function modificar_datos_me(){
-    cargarCentros();
-    cargarEspecialidades();
+    cargarCentros1();
+    cargarEspecialidades1();
     cambiarSeccion("cambiar_datos_me");
+
+    obtenerDatosMedico(medico, function(medico_pasado2){
+        document.getElementById("nombre1").value = medico_pasado2.nombre;
+        document.getElementById("apellidos1").value = medico_pasado2.apellidos;
+        document.getElementById("loginME2").value = medico_pasado2.login;
+        document.getElementById("centros1").value = medico_pasado2.centro;
+        document.getElementById("especialidad1").value = medico_pasado2.especialidad;
+    })
 }
 
 // Esta función carga los centros en el select
-function cargarCentros() {
+function cargarCentros1() {
     obtenerCentros(function(centros) {
         var selectCentros = document.getElementById("centros1");
         // Limpiamos cualquier opción previa
@@ -151,7 +167,7 @@ function cargarCentros() {
 }
 
 // Esta función carga las especialidades en el select
-function cargarEspecialidades() {
+function cargarEspecialidades1() {
     obtenerEspecialidades(function(especialidades) {
         var selectEspecialidades = document.getElementById("especialidad1");
         // Limpiamos cualquier opción previa
@@ -186,18 +202,325 @@ function guardar_datos_modificados(){
     }
 
     actualizarme(medico, cambios_me, function(medico){
+        if (!medico){
+            alert("El medico que se quiere actualizar no existe");
+            return;
+        }
         cambiarSeccion("menu_principal");
         console.log("datos del medico actualizados", medico)
-    })
+    });
 
     
 }
 
+function boton_volver_inicio(){
+    cambiarSeccion("menu_principal");
+}
 
+//boton para volver al login.
 function volver_login(){
     cambiarSeccion("login_me");
 }
 
+//Array de expedientes sin ME y de esa especialidad (solo id, map, fecha_creacion)
+//Para asignar un expediente
+function asignar_expediente(){
+    var lista_expedientes_disponibles = document.getElementById("lista_expedientes");
+    lista_expedientes_disponibles.innerHTML = "";
+
+    var tabla = document.createElement("table");
+    lista_expedientes_disponibles.appendChild(tabla);
+
+    var encabezados = ["ID", "F.Cre.", "MAP", ""];
+    var filaEncabezado = tabla.insertRow();
+
+    for(var i = 0; i < encabezados.length; i++){
+        var encabezado = document.createElement("th");
+        encabezado.innerHTML = encabezados[i];
+        filaEncabezado.appendChild(encabezado);
+    }
+
+    //medico = solo el id
+    obtenerDatosMedico(medico, function(medico_ME){
+        // Verificar si se pudo obtener el médico
+        if (medico_ME === null) {
+            console.log("Error: No se pudo obtener el médico.");
+            return;
+        }
+        // Obtener el ID de la especialidad del médico
+        var id_especialidad = medico_ME.especialidad;
+        console.log("ID de la especialidad:", id_especialidad);
+
+
+        obtenerExpDisponibles(id_especialidad, function(listado){
+            if(listado == null){
+                console.log("Error");
+            }
+            for(var i = 0; i < listado.length; i++){
+                var fila = tabla.insertRow();
+                var datos = [
+                    listado[i].id,
+                    transformar_fecha_hora(listado[i].fecha_creacion),
+                    listado[i].nombre_map
+                ];
+    
+                for(var j = 0; j < datos.length; j++){
+                    var celda = fila.insertCell(j);
+                    celda.innerHTML = datos[j];
+                }
+    
+                var celdaBoton = fila.insertCell(datos.length);
+                celdaBoton.innerHTML = "<button onclick='asignado(" + listado[i].id + ")'>Asignar</button>";
+            }
+        });
+
+    });
+    
+    cambiarSeccion("asignar_expediente");
+}
+
+function boton_volver_menuP(){
+    cambiarSeccion("menu_principal");
+}
+
+
+function cargarEspecialidades2() {
+    obtenerEspecialidades(function(especialidades) {
+        var selectEspecialidades = document.getElementById("especialidades2");
+        // Limpiamos cualquier opción previa
+        selectEspecialidades.innerHTML = "";
+        // Iteramos sobre las especialidades y las agregamos como opciones al select
+        especialidades.forEach(function(especialidad) {
+            var option = document.createElement("option");
+            option.value = especialidad.id; // Puedes cambiar esto según la estructura de tu objeto especialidad
+            option.text = especialidad.nombre; // Puedes cambiar esto según la estructura de tu objeto especialidad
+            selectEspecialidades.appendChild(option);
+        });
+    });
+}
+
+
+
+
+function asignado(id){
+    cargarEspecialidades2();
+    obtenerDatosMedico(medico, function(medico_pasado){
+        // Verificar si se pudo obtener el médico
+        if (medico_pasado === null) {
+            console.log("Error: No se pudo obtener el médico.");
+            return;
+        }
+        // Obtener el ID de la especialidad del médico
+        var id_me = medico_pasado.id;
+        console.log("ID del medico:", id_me);
+
+        asignarExp(id, id_me, function(exito){
+            if(exito){
+               cambiarSeccion("expediente_asignado"); 
+               
+               var fechaActual = new Date().toISOString();
+               document.getElementById("fasignacion").value = fechaActual;
+
+            }
+        
+        obtenerExpAsignados(id_me, function(expedientes){
+            expedientesAsignados = expedientes;
+            for (var i = 0; i < expedientesAsignados.length; i++) {
+            // Comparar el ID del expediente con el ID recibido como parámetro
+                if (expedientesAsignados[i].id === id) {
+                // Aquí encontraste el expediente que deseas mostrar
+                var expediente = expedientesAsignados[i];
+                console.log("Expediente encontrado:", expediente);
+                console.log(expediente);
+                document.getElementById("id1").value = expediente.id;
+                document.getElementById("medEsp").value = expediente.me;
+                document.getElementById("especialidades2").value = expediente.especialidad;
+                document.getElementById("sip").value = expediente.sip;
+                document.getElementById("nombrenuevoexp").value = expediente.nombre;
+                document.getElementById("apellidosnuevoexp").value = expediente.apellidos;
+                document.getElementById("fnacimientonuevoexp").value = expediente.fecha_nacimiento;
+                document.getElementById("genero").value = expediente.genero;
+                document.getElementById("observaciones").value = expediente.observaciones;
+                document.getElementById("solicitud").value = expediente.solicitud;
+                document.getElementById("respuesta").value = expediente.respuesta;
+                document.getElementById("fsolicitud").value = expediente.fecha_creacion;
+                document.getElementById("fasignacion").value = expediente.fecha_asignacion;
+                document.getElementById("fresolucion").value = expediente.fecha_resolucion;
+
+                }
+            }
+            
+        });
+
+        });
+    });
+}
+
+function boton_volver_listado(){
+    cambiarSeccion("asignar_expediente");
+}
+
+function mostrar_expedientes_asignados(){
+    var lista_expedientes = document.getElementById("lista_expedientes_asignados");
+    lista_expedientes.innerHTML = "";
+
+    // Crear tabla
+    var tabla = document.createElement("table");
+    lista_expedientes.appendChild(tabla);
+
+    // Encabezados de la tabla
+    var encabezados = ["ID", "F.Cre.", "F.Asg.", "F.Res.", "SIP", ""];
+    var filaEncabezado = tabla.insertRow();
+
+    for (var i = 0; i < encabezados.length; i++) {
+        var encabezado = document.createElement("th");
+        encabezado.innerHTML = encabezados[i];
+        filaEncabezado.appendChild(encabezado);
+    }
+
+    if(expedientesAsignados == null){
+        console.log("Error");
+    }
+    for(var i = 0; i < expedientesAsignados.length; i++){
+        var fila = tabla.insertRow();
+        var datos = [
+            expedientesAsignados[i].id,
+            transformar_fecha_hora(expedientesAsignados[i].fecha_creacion),
+            transformar_fecha_hora(expedientesAsignados[i].fecha_asignacion),
+            transformar_fecha_hora(expedientesAsignados[i].fecha_resolucion),
+            expedientesAsignados[i].sip
+        ];
+            
+        for(var j = 0; j < datos.length; j++){
+            var celda = fila.insertCell(j);
+            celda.innerHTML = datos[j];
+        }
+            
+        var celdaBoton = fila.insertCell(datos.length);
+        celdaBoton.innerHTML = "<button onclick='mostrar(" + expedientesAsignados[i].id + ")'>Mostrar</button>";
+        }
+
+    cambiarSeccion("expedientes_asignados");
+}
+
+function cargarEspecialidades3() {
+    obtenerEspecialidades(function(especialidades) {
+        var selectEspecialidades = document.getElementById("especialidades3");
+        // Limpiamos cualquier opción previa
+        selectEspecialidades.innerHTML = "";
+        // Iteramos sobre las especialidades y las agregamos como opciones al select
+        especialidades.forEach(function(especialidad) {
+            var option = document.createElement("option");
+            option.value = especialidad.id; // Puedes cambiar esto según la estructura de tu objeto especialidad
+            option.text = especialidad.nombre; // Puedes cambiar esto según la estructura de tu objeto especialidad
+            selectEspecialidades.appendChild(option);
+        });
+    });
+}
+var idExpResolver=null
+//aqui tambien tengo que cargar todos los datos del expediente y la especialidad...
+function mostrar(id_expediente){
+    cambiarSeccion("ver_expediente_asignado");
+    cargarEspecialidades3();
+    idExpResolver = id_expediente;
+    console.log("este es el id del expediente a resolver:", idExpResolver);
+    // Iterar sobre el array de expedientes
+    for (var i = 0; i < expedientesAsignados.length; i++) {
+        // Comparar el ID del expediente con el ID recibido como parámetro
+        if (expedientesAsignados[i].id === id_expediente) {
+        // Aquí encontraste el expediente que deseas mostrar
+        var expediente = expedientesAsignados[i];
+        console.log("Expediente encontrado:", expediente);
+
+        document.getElementById("id2").value = expediente.id;
+        document.getElementById("medEsp1").value = expediente.me;
+        document.getElementById("especialidades3").value = expediente.especialidad;
+        document.getElementById("sip1").value = expediente.sip;
+        document.getElementById("nombre2").value = expediente.nombre;
+        document.getElementById("apellidos2").value = expediente.apellidos;
+        document.getElementById("fnacimiento2").value = expediente.fecha_nacimiento;
+        document.getElementById("genero2").value = expediente.genero;
+        document.getElementById("observaciones2").value = expediente.observaciones;
+        document.getElementById("solicitud2").value = expediente.solicitud;
+        document.getElementById("respuesta2").value = expediente.respuesta;
+        document.getElementById("fsolicitud2").value = expediente.fecha_creacion;
+        document.getElementById("fasignacion2").value = expediente.fecha_asignacion;
+        document.getElementById("fresolucion2").value = expediente.fecha_resolucion;
+
+        // Una vez que encuentras el expediente que deseas mostrar, puedes salir del bucle
+        break;
+        }
+    }
+}
+
+
+//Resuelve un expediente poniendo la respuesta y asignado la fecha_resolucion.
+function resolverExpediente(){
+    respuesta = document.getElementById("respuesta2").value;
+// Resolver el expediente seleccionado
+    resolverExp(idExpResolver, respuesta, function(respuestaServer){
+        if(respuestaServer){  
+            var fechaActual = new Date().toISOString();
+            document.getElementById("fresolucion2").value = fechaActual;
+            console.log(fechaActual);
+            console.log("Expediente resuelto con exito"); 
+
+            asignado();
+        }
+        else{
+            console.log("expediente no encontrado");
+        }
+
+        cambiarSeccion("expedientes_asignados");
+    });
+}
+
+
+function boton_volver_listado2(){
+    cambiarSeccion("expedientes_asignados");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function borrar_campos_login(){
+    document.getElementById("loginME").value = '';
+    document.getElementById("passwordME").value = '';
+}
+
+function borrar_campos_registro(){
+    document.getElementById("nombre").value = '';
+    document.getElementById("apellidos").value = '';
+    document.getElementById("loginME1").value = '';
+    document.getElementById("passwordME1").value = '';
+    document.getElementById("centros").value = '';
+    document.getElementById("especialidad").value = '';
+}
 
 
 
@@ -274,6 +597,24 @@ function cambiarSeccion(seccion){
     document.getElementById(seccionActual).classList.remove("activa");
     document.getElementById(seccion).classList.add("activa");
     seccionActual=seccion;
+}
+
+//FUNCION PARA CAMBIAR FECHA A UNA MAS LEGIBLE
+function transformar_fecha(fecha){
+    var año = String(fecha).substring(0,4);
+    var mes = String(fecha).substring(5,7);
+    var dia = String(fecha).substring(8,10);
+    return año+"-"+mes+"-"+dia;
+}
+
+//Para las horas
+function transformar_fecha_hora(fecha){
+    var horas = String(fecha).substring(11,13);
+    var minutos = String(fecha).substring(14,16);
+    var segundos = String(fecha).substring(17,19);
+
+    return transformar_fecha(fecha)+ " "+horas+":"+minutos+":"+segundos;
+
 }
 
 
